2a0cac9e9ab77a32b2e7283731b1a2d169cd6880
Clea Monique Sacriz <clea.sacriz@deped.gov.ph>
2026-02-11 16:37:29 +0800

    feb 11 fix specialization

--- src/forms/TeacherSpecialization.jsx
+++ src/forms/TeacherSpecialization.jsx
@@ -22,8 +22,8 @@ const getInitialFields = () => ({
     spec_esp_major: 0, spec_esp_teaching: 0,
     spec_tle_major: 0, spec_tle_teaching: 0,
     // General Education (For Purely Elementary)
-    spec_general_major: 0,
-    spec_ece_major: 0,
+    spec_general_major: 0, spec_general_teaching: 0,
+    spec_ece_major: 0, spec_ece_teaching: 0,
     // New Secondary Fields
     spec_bio_sci_major: 0, spec_bio_sci_teaching: 0,
     spec_phys_sci_major: 0, spec_phys_sci_teaching: 0,
